87a22a506f2df20087c7a1a51a057b253eb25532
SJAljawhary <sndjehad@gmail.com>
2021-07-05 15:36:34 +0300

    editing

--- src/App.js
+++ src/App.js
@@ -5,73 +5,90 @@ import './App.css';
 
 class App extends React.Component {
 
-  constructor (props){
-      super(props);
-      this.state={
-        cityName : '',
-        cityData : {},
-        cityMap : false
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: '',
+      cityData: {},
+      cityMap: false,
+      errorMessage: false
+
+    }
   }
 
   setLocation = async (event) => {
 
-    event.preventDefault ();
+    event.preventDefault();
 
-    this.setState({
-      cityName : event.target.city.value
+    await this.setState({
+      cityName: event.target.city.value
 
     })
+
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&city=${this.state.cityName}&format=json`;
+
+
+    try {
+      let recievedData = await axios.get(url);
+
+      console.log(recievedData);
+      console.log(recievedData.data[0]);
+
+      this.setState({
+        cityData: recievedData.data[0],
+        cityMap: true
+      })
   
-    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&city=${this.state.cityName}&format=json`
 
-     let recievedData = await axios.get(url);
+    } catch {
+      this.setState({
+        errorMessage: true
+      })
 
-     this.setState({
-       cityData : recievedData.data[0],
-       cityMap : true 
-     })
-     console.log(recievedData);
-     console.log(recievedData.data[0]);
+    }
   }
 
-  render() {
-    return (
-      <div>
+    render() {
+      return (
+        <div>
 
-        <h1 className='header'>City Explorer</h1>
-        <form onSubmit={this.setLocation} className='cityInfo'>
+          <h1 className='header'>City Explorer</h1>
+          <form onSubmit={this.setLocation} className='cityInfo'>
 
-          <input className='cityName' type='text' placeholder='city name' name='city'/><br></br>
-          <input className='button' type='submit' value='Explore!'/>
-      
-        </form>
+            <input className='cityName' type='text' placeholder='city name' name='city' /><br></br>
+            <input className='button' type='submit' value='Explore!' />
+
+          </form>
 
           <div className='information'>
 
-          <p>City Name : {this.state.cityData.display_name}</p>
-          <p>Lattitude : {this.state.cityData.lat}</p>
-          <p>Longitude : {this.state.cityData.lon}</p>
+            <p>City Name : {this.state.cityData.display_name}</p>
+            <p>Lattitude : {this.state.cityData.lat}</p>
+            <p>Longitude : {this.state.cityData.lon}</p>
 
           </div>
 
-
-
           {
-            this.state.cityMap && 
+            this.state.cityMap &&
 
-        <img className='map' alt='' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=10`} />
+            <img className='map' alt='' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=10`} />
 
           }
 
+          {this.state.errorMessage &&
+            <p>something went wrong in getting data from locationiq ! </p>
+          }
 
-      </div>
 
-    )
+        </div>
+
+      )
+    }
+
+
+
   }
 
 
 
-}
-
-export default App;
+  export default App;
